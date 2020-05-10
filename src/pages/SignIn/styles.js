a48de43b3518968b359be326bdef8749cdf4b3e3
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-05-10 16:58:58 -0300

    more styles

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -2,18 +2,27 @@ import styled from 'styled-components/native';
 
 
 export const Background = styled.View`
-
+flex: 1;
+background-color: #131313;
 `;
 
 export const Container = styled.KeyboardAvoidingView`
-
+flex:1;
+align-items: center;
+justify-content: center;
 `;
 export const Logo = styled.Image`
-
+margin-top: 15px;
 `;
 export const AreaInput = styled.View`
-
+flex-direction: row;
 `;
 export const Input = styled.TextInput`
-
+background: rgba(0,0,0,0.20);
+width: 90%;
+font-size: 17px;
+color: #FFF;
+margin-bottom: 15px;
+padding: 10px;
+border-radius: 7px;
 `;
