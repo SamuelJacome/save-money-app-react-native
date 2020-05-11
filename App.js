48d38709b904057f412abe037d0ab310669ca792
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-05-11 11:56:32 -0300

    add context

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native'
-import firebase from './src/services/firebase';
 import { StatusBar } from 'react-native';
+import AuthProvider from './src/contexts/auth';
+
 import Routes from './src/routes/index'
-// import { Container } from './styles';
 
 function App ()  {
   return (
     <NavigationContainer>
-      <StatusBar backgroundColor="#131313" barStyle="light-content"/>
-      <Routes/>
+      <AuthProvider>      
+        <StatusBar backgroundColor="#131313" barStyle="light-content"/>
+        <Routes/>
+      </AuthProvider>
     </NavigationContainer>
   
   );
