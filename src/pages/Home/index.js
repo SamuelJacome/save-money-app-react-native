ba6e7e25f4e2a159fe356a49d76d19e571266372
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-05-13 16:34:15 -0300

    signOut ok

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,18 @@
 import React, {useContext} from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { AuthContext } from '../../contexts/auth';
 
 // import { Container } from './styles';
 
 function Home () {
-  const {user} = useContext(AuthContext)
+  const {user, signOut} = useContext(AuthContext)
   return (
   
   <View>
     <Text>Home</Text>
     <Text>{user && user.nome}</Text>
     <Text>{user && user.email}</Text>
+    <Button title="Sair da Conta" onPress={()=> signOut()}/>
   </View>
   
   
