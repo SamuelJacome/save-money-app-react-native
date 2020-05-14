e93ac28948a71c3169d22bc97528613d8002dbec
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-05-13 21:33:39 -0300

    att

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+function Header  ()  {
+  return (
+  
+    <View>
+
+        
+
+    </View>
+      
+ );
+}
+
+export default Header;
