48636c57038c2bc20d41d314e846d342c4f4357c
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-05-17 23:44:13 -0300

    att

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-
-// import { Container } from './styles';
+import React, {useState} from 'react';
+import { SafeAreaView, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import Header from '../../components/Header'
+import {  Background, Input, SubmitButton, SubmitText } from './styles';
 
 const New = () => {
-  return (
-  
-  <View >
-      <Text>New</Text>
 
-  </View>
+  const[valor, setValor] = useState('');
+  const[tipo, setTipo] = useState(null);
+  return (
+  <TouchableWithoutFeedback onPress={()=> Keyboard.dismiss()}>
+  <Background >
+      <Header/>
+
+      <SafeAreaView style={{alignItems: 'center'}}>
+        <Input 
+        placeholder="Valor Desejado" 
+        keyboardType="numeric" 
+        returnKeyType="next"
+        onSubmitEditing = { ()=> Keyboard.dismiss() }
+        />
+      
+        <SubmitButton>
+          <SubmitText>Registrar</SubmitText>
+        </SubmitButton>
+      
+      </SafeAreaView>
+
+  </Background>
+
+  </TouchableWithoutFeedback>
   );
 }
 
