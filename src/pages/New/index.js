246cf5ac6972a5023de9e33b1192e6f45e13bb3a
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-05-18 13:40:38 -0300

    New component ok

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,12 +1,19 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import { SafeAreaView, Keyboard, TouchableWithoutFeedback, Alert } from 'react-native';
+import {format} from 'date-fns'
+import { useNavigation } from '@react-navigation/native'
+import firebase from '../../services/firebase';
+import { AuthContext } from '../../contexts/auth'
 import Header from '../../components/Header'
 import {  Background, Input, SubmitButton, SubmitText } from './styles';
 import Picker from '../../components/Picker'
 function New () {
 
+  const navigation = useNavigation();
   const[valor, setValor] = useState('');
   const[tipo, setTipo] = useState(null);
+
+  const {user: usuario} = useContext(AuthContext)
   
   function handleSubmit(){
     Keyboard.dismiss();
@@ -17,7 +24,7 @@ function New () {
 
     Alert.alert(
       'Confirmando Dados',
-      `Tipo ${tipo} - Valor: ${parseFloat(valor)}`,
+      `Tipo: ${tipo} - Valor: ${parseFloat(valor)}`,
       [
         {
           text: 'Cancelar',
@@ -32,10 +39,31 @@ function New () {
   }
   
 
-  function handleAdd(){
-    alert("Adicionou!")
+  async function handleAdd(){
+    let uid = usuario.uid;
+ 
+     let key = await firebase.database().ref('historico').child(uid).push().key;
+     await firebase.database().ref('historico').child(uid).child(key).set({
+       tipo: tipo,
+       valor: parseFloat(valor),
+       date: format(new Date(), 'dd/MM/yy')
+     })
+ 
+     //Atualizar o nosso saldo
+     let user = firebase.database().ref('users').child(uid);
+     await user.once('value').then((snapshot)=>{
+       let saldo = parseFloat(snapshot.val().saldo);
+ 
+       tipo === 'despesa' ? saldo -= parseFloat(valor) : saldo += parseFloat(valor);
+ 
+       user.child('saldo').set(saldo);
+ 
+     });
+     Keyboard.dismiss();
+     setValor('');
+     navigation.navigate('Home');
+ 
   }
-  
   return (
   <TouchableWithoutFeedback onPress={()=> Keyboard.dismiss()}>
   <Background >
